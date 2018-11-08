4d74eac36245e7a71ed95609a97609aa5aa07157
alvinussutendy <alvinussutendy@gmail.com>
2018-11-08 19:27:57 +0700

    Commit 2

---
+++ Calculator - Copy.java
@@ -0,0 +1,22 @@
+public class Calculator{
+	
+	public float plus(float a, float b){
+		return a+b;
+		
+	}
+	
+	public float minus(float a, float b){
+		return a-b;
+		
+	}
+	
+	public float multiply(float a, float b){
+		return a*b;
+		
+	}
+	
+	public float division(float a, float b){
+		return a/b;
+		//tes
+	}
+}
